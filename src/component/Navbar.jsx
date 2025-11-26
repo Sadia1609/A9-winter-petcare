3448bf37f44a771d0bc58ee7a9615e716773c8d9
Sadia1609 <sadia.rahman160920@gmail.com>
2025-11-26 18:51:10 +0600

    added extra home sections

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -22,21 +22,13 @@ const Navbar = () => {
         <li><a>Item 3</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">PetCare</a>
+    <a className="btn btn-ghost text-xl">Pet Paw</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item Changed</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      <li><a>Home</a></li>
+      <li><a>Services</a></li>
+      <li><a>My Profile</a></li>
     </ul>
   </div>
   <div className="navbar-end">
