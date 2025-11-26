f5544b9cc9c573d67602a540500fd42506c3e606
Sadia1609 <sadia.rahman160920@gmail.com>
2025-11-26 22:50:52 +0600

    update profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../Provider/AuthProvider';
+import auth from '../firebase/firebase.config';
+import { updateProfile } from 'firebase/auth';
 
 const Profile = () => {
+
+    const {setUser,user} = useContext(AuthContext)
+   
+    const[isOpen, setIsOpen] = useState(false)
+
+    const handleOpenForm = () =>{
+
+       
+        setIsOpen(!isOpen)
+        
+    }
+
+    const handleUpdate = (e)=>{
+        e.preventDefault();
+        const name = e.target.name.value;
+        const photoUrl = e.target.photoUrl.value;
+
+         updateProfile(auth.currentUser, {
+  displayName:name, photoURL: photoUrl
+}).then(() => {
+ 
+ setUser({...user,photoURL:photoUrl, displayName:name })
+}).catch((error) => {
+  console.log(error)
+  
+});
+    }
+
+
     return (
-        <div>
-            profile
+        <div className='flex flex-col justify-center items-center'>
+           <div className="avatar">
+    <div className="w-12">
+      <img src={user?.photoURL} />
+    </div>
+  </div>
+  <p>{user?.displayName}</p>
+  <p>{user?.email}</p>
+  <button onClick={handleOpenForm} className='btn'>Update Profile</button>
+  
+
+        {
+            isOpen && (
+                 <form onSubmit={handleUpdate} className="fieldset">
+          <label className="label">Name</label>
+          <input defaultValue={user?.displayName} name='name' type="text" className="input" placeholder="your name" />
+
+          <label className="label">Photo Url</label>
+          <input defaultValue={user?.photoURL} name='photoUrl' type="text" className="input" placeholder="your url" />
+          
+
+       
+          <button className="btn btn-neutral mt-4">Update</button>
+        </form>
+            )
+        }
+
         </div>
     );
 };
